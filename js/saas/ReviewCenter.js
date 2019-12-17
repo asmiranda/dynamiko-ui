4e80a5514fabc340f1f82705e1994f200e6e9a3b
niq77avie@yahoo.com <niq77avie@yahoo.com>
2019-12-17 12:54:18 +0800

    1

--- js/saas/ReviewCenter.js
+++ js/saas/ReviewCenter.js
@@ -10,12 +10,49 @@ class ReviewCenter {
         $(document).on('click', '.btnDeleteQuestion', function(){
             context.deleteQuestion(this);
         });
+        $(document).on('keyup', '.txtSearchQuestion', function(){
+            context.searchQuestion(this);
+        });
+    }
+
+    searchQuestion(obj) {
+        var context = this;
+
+        var examId = $(obj).attr("examId");
+        var value = $(obj).val();
+        console.log(value);
+        var url = MAIN_URL+'/api/generic/'+localStorage.companyCode+'/widget/ReviewModuleUI/searchQuestions/'+examId+"/"+value;
+        var ajaxRequestDTO = new AjaxRequestDTO(url, "");
+        var successCallback = function(data) {
+            console.log("Record Found");
+            console.log(data);
+            $(".addQuestionList").empty();
+            $.each(data, function(index, obj) {
+                var question = obj.getProp("question");
+                var answer = obj.getProp("answer");
+                var choiceA = obj.getProp("choiceA");
+                var choiceB = obj.getProp("choiceB");
+                var choiceC = obj.getProp("choiceC");
+                var choiceD = obj.getProp("choiceD");
+                var examQuestionId = obj.getProp("ReviewQuestionAnswerId");
+                var qHtml = `
+                    <li class="list-group-item">
+                        <b>${question}</b> <a class="pull-right btnAddQuestion" examId="${examId}" examQuestionId="${examQuestionId}" title="${examId} - ${examQuestionId}"><i class="fa fa-fw fa-plus-square"></i> Add</a>
+                    </li>
+                `;
+                $(".addQuestionList").append(qHtml);
+            });
+        };
+        var ajaxCaller = new AjaxCaller(ajaxRequestDTO, successCallback);
+        ajaxCaller.ajaxGet();
     }
 
     showQuestions(obj) {
         var context = this;
 
         var examId = $(obj).attr("examId");
+        $("input.txtSearchQuestion").attr("examId", examId);
+
         var title = $(obj).attr("title");
         $(".examTitle").html(title+" ["+examId+"]");
         var url = MAIN_URL+'/api/generic/'+localStorage.companyCode+'/widget/ReviewModuleUI/loadQuestions/'+examId;
@@ -41,6 +78,36 @@ class ReviewCenter {
     }
 
     addQuestion(obj) {
+        var context = this;
+
+        var examId = $(obj).attr("examId");
+        var examQuestionId = $(obj).attr("examQuestionId");
+        var txtSearch = $(".txtSearchQuestion").val();
+
+        var url = MAIN_URL+'/api/generic/'+localStorage.companyCode+'/widget/ReviewModuleUI/addQuestion/'+examId+"/"+examQuestionId+"/"+txtSearch;
+        var ajaxRequestDTO = new AjaxRequestDTO(url, "");
+        var successCallback = function(data) {
+            console.log("Record Found");
+            console.log(data);
+            $(".addQuestionList").empty();
+            $.each(data, function(index, obj) {
+                var question = obj.getProp("question");
+                var answer = obj.getProp("answer");
+                var choiceA = obj.getProp("choiceA");
+                var choiceB = obj.getProp("choiceB");
+                var choiceC = obj.getProp("choiceC");
+                var choiceD = obj.getProp("choiceD");
+                var examQuestionId = obj.getProp("ReviewQuestionAnswerId");
+                var qHtml = `
+                    <li class="list-group-item">
+                        <b>${question}</b> <a class="pull-right btnAddQuestion" examId="${examId}" examQuestionId="${examQuestionId}" title="${examId} - ${examQuestionId}"><i class="fa fa-fw fa-plus-square"></i> Add</a>
+                    </li>
+                `;
+                $(".addQuestionList").append(qHtml);
+            });
+        };
+        var ajaxCaller = new AjaxCaller(ajaxRequestDTO, successCallback);
+        ajaxCaller.ajaxGet();
     }
 
     deleteQuestion(obj) {
