87a7f712431fdf304b7b5caf5a55fdf72ff777d4
Alex Miranda <aiamemiranda@gmail.com>
2020-06-06 00:55:25 +0800

    In Progress - Conference 360

---
+++ js/meetingroom copy/roomSignal.js
@@ -0,0 +1,75 @@
+class RoomSignal {
+    constructor() {
+        this.conCompany;
+        this.conRoom;
+        this.sockjs;
+        this.messageCallback;
+
+        var context = this;
+    }
+
+    send(action, data) {
+        console.log(`send signal ${action}`);
+        var tmp = {};
+        tmp["action"] = action;
+        tmp["from"] = USERNAME;
+        tmp["data"] = data;
+        this.sockjs.send(JSON.stringify(tmp));
+    }
+
+    initConnectionListeners() {
+        context.sockjs.onopen = function () {
+            console.log("Connected to the signaling server");
+        };
+        context.sockjs.onclose = function () {
+            console.log('close');
+        };
+        // context.sockjs.onmessage = context.messageCallback;
+
+        context.sockjs.onmessage = function (msg) {
+            console.log("Got message", msg.data, msg);
+            // var content = JSON.parse(msg.data);
+            // var data = content.data;
+            // switch (content.event) {
+            //     // when somebody wants to call us
+            //     case "offer":
+            //         context.handleOffer(data);
+            //         break;
+            //     case "answer":
+            //         context.handleAnswer(data);
+            //         break;
+            //     // when a remote peer sends an ice candidate to us
+            //     case "candidate":
+            //         context.handleCandidate(data);
+            //         break;
+            //     default:
+            //         break;
+            // }
+        };
+    }
+
+    joinRoom(conCompany, conRoom, messageCallback) {
+        var context = this;
+        context.conCompany = conCompany;
+        context.conRoom = conRoom;
+
+        context.messageCallback = messageCallback;
+        context.sockjs = new SockJS(`${MAIN_URL}/socket/${context.conCompany}/${context.conRoom}`);
+        context.sockjs.onopen = function () {
+            console.log("Connected to the signaling server");
+            context.send("join", USERNAME);
+        };
+        context.sockjs.onclose = function () {
+            console.log('close');
+        };
+        // context.sockjs.onmessage = context.messageCallback;
+
+        context.sockjs.onmessage = function (msg) {
+            console.log("Got message", msg.data, msg);
+        };
+    }
+}
+
+$(function () {
+    roomSignal = new RoomSignal();
+});
--- js/meetingroom/meetingRoom.js
+++ js/meetingroom/meetingRoom.js
@@ -1,172 +1,64 @@
 class MeetingRoom {
     constructor() {
-        this.peerConnectionConfig = {
-            'iceServers': [
-                { 'urls': 'stun:stun.services.mozilla.com' },
-                { 'urls': 'stun:stun.l.google.com:19302' },
-            ]
-        };
-        this.mediaConstraints = window.constraints = {
-            audio: false,
-            video: true
-        };
+    }
 
-        this.localStream;
-        this.peerConnection;
-        this.dataChannel;
-        this.input = document.getElementById("messageInput");
-        this.sockjs = new SockJS(`${MAIN_URL}/socket/C1/R1`);
-
+    messageCallback(msg) {
         var context = this;
-        context.sockjs.onopen = function () {
-            console.log("Connected to the signaling server");
-        };
-        context.sockjs.onclose = function () {
-            console.log('close');
-        };
-        context.sockjs.onmessage = function (msg) {
-            console.log("Got message", msg.data);
-            var content = JSON.parse(msg.data);
-            var data = content.data;
-            switch (content.event) {
-                // when somebody wants to call us
-                case "offer":
-                    context.handleOffer(data);
-                    break;
-                case "answer":
-                    context.handleAnswer(data);
-                    break;
-                // when a remote peer sends an ice candidate to us
-                case "candidate":
-                    context.handleCandidate(data);
-                    break;
-                default:
-                    break;
-            }
-        };
+        console.log("Got message", msg.data);
+        var jsonMsg = JSON.parse(msg.data);
 
-        $(document).on('click', `.btnSendChat`, function () {
-            context.sendMessage();
-        });
-        $(document).on('click', `.btnShareVideo`, function () {
-            context.shareVideo();
-        });
-    }
-
-    shareVideo() {
-        var context = this;
-
-        const activeVideo = document.getElementById('activeVideo');
-        activeVideo.srcObject = context.localStream;
-
-        const myVideo = document.querySelector('video#myVideo');
-        myVideo.srcObject = context.localStream;
-
-        context.peerConnection.addStream(context.localStream);  
-    }
-
-    send(message) {
-        this.sockjs.send(JSON.stringify(message));
-    }
-
-    displayRemoteStream(event) {
-        var video = document.querySelectorAll('video#remoteVideo');
-
-        // video.src = window.URL.createObjectURL(event.stream);
-        video.srcObject = event.stream;
-        video.muted = true;
-    }
-
-    initP2P() {
-        var context = this;
-
-        context.peerConnection = new RTCPeerConnection(context.peerConnectionConfig, {
-            optional: [{
-                RtpDataChannels: true
-            }]
-        });
-
-        // Setup ice handling
-        context.peerConnection.onicecandidate = function (event) {
-            if (event.candidate) {
-                context.send({
-                    event: "candidate",
-                    data: event.candidate
-                });
-            }
-        };
-
-        context.peerConnection.onaddstream = function () {
-            context.displayRemoteStream(event)
+        var action = jsonMsg.action;
+        var from = jsonMsg.from;
+        var data = jsonMsg.data;
+        if ("user-joined"==action) {
+            meetingRoom.userJoined(from, data);
         }
-        context.peerConnection.addStream(context.localStream);  
+        else if ("active-users"==action) {
+            meetingRoom.activeUsers(from, data);
+        }
+    }
 
-        // creating data channel
-        console.log("Create Data Channel");
-        context.dataChannel = context.peerConnection.createDataChannel("dataChannel", {
-            reliable: true
-        });
-
-        context.dataChannel.onerror = function (error) {
-            console.log("Error occured on datachannel:", error);
-        };
-
-        // when we receive a message from the other peer, printing it on the console
-        context.dataChannel.onmessage = function (event) {
-            console.log("message:", event.data);
-            var value = $("textarea#allChatMessages").val();
-            value += event.data;
-            $("textarea#allChatMessages").val(value);
-        };
-
-        context.dataChannel.onclose = function () {
-            console.log("data channel is closed");
-        };
-
-        context.peerConnection.createOffer(function (offer) {
-            context.send({
-                event: "offer",
-                data: offer
-            });
-            context.peerConnection.setLocalDescription(offer);
-        }, function (error) {
-            alert("Error creating an offer");
+    activeUsers(from, data) {
+        console.log(data);
+        $(".selectChatTo").empty();
+        $(".selectChatTo").append(`<option value="All">All</option>`);
+        $(data).each(function(index, str) {
+            var opt = `<option value="${str.key}">${str.value}</option>`;
+            $(".selectChatTo").append(opt);
         });
     }
 
-    handleOffer(offer) {
+    userJoined(from, data) {
         var context = this;
-        context.peerConnection.setRemoteDescription(new RTCSessionDescription(offer));
-
-        // create and send an answer to an offer
-        context.peerConnection.createAnswer(function (answer) {
-            context.peerConnection.setLocalDescription(answer);
-            context.send({
-                event: "answer",
-                data: answer
-            });
-        }, function (error) {
-            alert("Error creating an answer");
+        var newlyJoinedProfile = "";
+        console.log(data);
+        $(".selectChatTo").empty();
+        $(".selectChatTo").append(`<option value="All">All</option>`);
+        $(data).each(function(index, str) {
+            var opt = `<option value="${str.key}">${str.value}</option>`;
+            $(".selectChatTo").append(opt);
+            if (str.key==from) {
+                newlyJoinedProfile =str.value;
+            }
+            context.updateVideoBox(str);
         });
+        if (newlyJoinedProfile!="") {
+            $("#messageAlert").html(newlyJoinedProfile+" joined!");
+        }
+    }
 
-    };
-
-    handleCandidate(candidate) {
-        var context = this;
-        context.peerConnection.addIceCandidate(new RTCIceCandidate(candidate));
-    };
-
-    handleAnswer(answer) {
-        var context = this;
-        context.peerConnection.setRemoteDescription(new RTCSessionDescription(answer));
-        console.log("connection established successfully!!");
-    };
-
-    sendMessage() {
-        var context = this;
-        var messageText = $('textarea#chatMessageTxt').val();
-        context.dataChannel.send(messageText);
-        $('textarea#chatMessageTxt').val("");
+    updateVideoBox(keyVal) {
+        var email = keyVal.key;
+        var profile = keyVal.value;
+        if (!$(`.miniVideo[email="${email}"]`).length) {
+            var str = `
+                <div style="flex: 1; width: 100px; display: flex; flex-direction: column;" class="miniVideo" email="${email}">
+                    <video class="miniVideoStream" email="${email}" style="width: 100px; min-height: 100px; max-height: 100px; background-color: cornflowerblue;" autoplay playsinline></video>
+                    <div class="text-center">${profile}</div>
+                </div>
+            `;
+            $(".videoBoxList").append(str);
+        }
     }
 
     join(roomName, conCompany, conRoom) {
@@ -178,51 +70,15 @@ class MeetingRoom {
         var context = this;
 
         var successRoomPopup = function (data) {
-            context.initAll(event);
+            roomSignal.joinRoom(conCompany, conRoom, context.messageCallback);
         }
         var successCallback = function (data) {
             showModalAny1200.show(title, data, successRoomPopup);
         };
         utils.getTabHtml("ConferenceUI", "MeetingRoom", successCallback);
     }
-
-    async initAll(e) {
-        var context = this;
-        try {
-            context.localStream = await navigator.mediaDevices.getUserMedia(context.mediaConstraints);
-
-            const video = document.getElementById("myVideo");
-            video.srcObject = context.localStream;
-
-            context.initP2P();
-        } catch (e) {
-            context.handleError(e);
-        }
-    }
-
-    handleError(error) {
-        var context = this;
-        if (error.name === 'ConstraintNotSatisfiedError') {
-            const v = constraints.video;
-            context.errorMsg(`The resolution ${v.width.exact}x${v.height.exact} px is not supported by your device.`);
-        } else if (error.name === 'PermissionDeniedError') {
-            context.errorMsg('Permissions have not been granted to use your camera and ' +
-                'microphone, you need to allow the page access to your devices in ' +
-                'order for the demo to work.');
-        }
-        context.errorMsg(`getUserMedia error: ${error.name}`, error);
-    }
-
-    errorMsg(msg, error) {
-        const errorElement = document.getElementById('errorMsg');
-        errorElement.innerHTML += `<p>${msg}</p>`;
-        if (typeof error !== 'undefined') {
-            console.error(error);
-        }
-    }
 }
 
 $(function () {
     meetingRoom = new MeetingRoom();
 });
-
