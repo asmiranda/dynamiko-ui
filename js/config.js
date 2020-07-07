7206c115c92a01a82e5fc364d750a65b1fea4ab9
alex <niq77avie@yahoo.com>
2020-07-07 22:12:53 +0800

    In Progress - Demo Site Notification

--- js/config.js
+++ js/config.js
@@ -1,12 +1,20 @@
+// var dynamikoPort = "8888";
+// var roomSignalPort = "7788";
+
 var server = "localhost";
-var MAIN_URL = `http://${server}:8888`;
-var MAIN_SIGNAL_URL = `ws://${server}:7788`;
-var MAIN_SIGNAL_HTTP_URL = `http://${server}:7788`;
+var dynamikoPort = "8443";
+var roomSignalPort = "4343";
+
+var MAIN_URL = `https://${server}:${dynamikoPort}`;
+var MAIN_SIGNAL_URL = `wss://${server}:${roomSignalPort}`;
+var MAIN_SIGNAL_HTTP_URL = `https://${server}:${roomSignalPort}`;
 
 // var server = "service.dynamikosoft.com";
-// var MAIN_URL = `https://${server}:8443`;
-// var MAIN_SIGNAL_URL = `wss://${server}:4343`;
-// var MAIN_SIGNAL_HTTP_URL = `https://${server}:4343`;
+// var dynamikoPort = "8443";
+// var roomSignalPort = "4343";
+// var MAIN_URL = `https://${server}:${dynamikoPort}`;
+// var MAIN_SIGNAL_URL = `wss://${server}:${roomSignalPort}`;
+// var MAIN_SIGNAL_HTTP_URL = `https://${server}:${roomSignalPort}`;
 
 var TOKEN;
 var USERNAME;
--- js/modules/submodules/NotificationUI.js
+++ js/modules/submodules/NotificationUI.js
@@ -1,20 +1,89 @@
 class NotificationUI extends AbstractSubUI {
     constructor() {
         super("NotificationUI");
+        var context = this;
+        $(document).on('click', `.btnReplyToNotification`, function() {
+            context.btnReplyToNotification(this);
+        });
+    }
+
+    btnReplyToNotification(obj) {
+        var message = $(`textarea[name='replyMessage']`).val();
+        console.log('btnReplyToNotification', message);
+
+        var context = this;
+        var recordId = $(obj).attr("recordId");
+        var tmp = {};
+        tmp["recordId"] = recordId;
+        tmp["message"] = message;
+
+        console.log(tmp);
+        var vdata = JSON.stringify(tmp); 
+
+        var url = `${MAIN_URL}/api/generic/${sessionStorage.companyCode}/widget/NotificationUI/post/replyMessageThread`;
+        var ajaxRequestDTO = new AjaxRequestDTO(url, vdata);
+
+        var successFunction = function(ret) {
+            console.log("btnReplyToNotification", url, ret);
+            context.arrangeNotificationThread(ret);
+        };
+        ajaxCaller.ajaxPost(ajaxRequestDTO, successFunction);
     }
 
     onProfileLoaded(data) {
         console.log("On Profile Loaded called.", data);
-
+        var context = this;
         var recordId = data.NotificationId;
+        $(".btnReplyToNotification").attr("recordId", recordId);
         var url = `${MAIN_URL}/api/generic/${sessionStorage.companyCode}/widget/NotificationUI/getMessageThread/${recordId}`;
         var ajaxRequestDTO = new AjaxRequestDTO(url, "");
 
         var successFunction = function(ret) {
             console.log("onProfileLoaded", url, ret);
-            $("#messageThread").html(ret.value);
+            context.arrangeNotificationThread(ret);
         };
         ajaxCaller.ajaxGet(ajaxRequestDTO, successFunction);
+    }
+
+    arrangeNotificationThread(data) {
+        $("#messageThread").empty();
+        $(data).each(function(index, obj) {
+            console.log(obj);
+            var messageType = obj.getPropDefault("messageType", "");
+            var message = obj.getPropDefault("message", "");
+            var strChat = "";
+            if  (messageType=="Receive") {
+                var fromEmail = obj.getPropDefault("fromEmail", "");
+                var receivedDate = obj.getPropDefault("receivedDate", "");
+                strChat = `
+                    <div class="direct-chat-msg">
+                        <div class="direct-chat-info clearfix">
+                            <span class="direct-chat-name pull-left">${fromEmail}</span>
+                            <span class="direct-chat-timestamp pull-right">${receivedDate}</span>
+                        </div>
+                        <div class="direct-chat-text">
+                            ${message}
+                        </div>
+                    </div>
+                `;
+            }
+            else {
+                var replyByEmail = obj.getPropDefault("replyByEmail", "");
+                var replyDate = obj.getPropDefault("replyDate", "");
+                strChat = `
+                    <div class="direct-chat-msg right">
+                        <div class="direct-chat-info clearfix">
+                            <span class="direct-chat-name pull-left">${replyByEmail}</span>
+                            <span class="direct-chat-timestamp pull-right">${replyDate}</span>
+                        </div>
+                        <div class="direct-chat-text">
+                            ${message}
+                        </div>
+                    </div>
+                `;
+            }
+            $("#messageThread").append(strChat);
+        });
     }
 
     newRecord() {   
@@ -27,7 +96,7 @@ class NotificationUI extends AbstractSubUI {
         var receivedDate = obj.getPropDefault("receivedDate", "");
         var message = obj.getPropDefault("message", "--");
         if (message.length>30) {
-            message = message.subStr(0, 30);
+            message = message.substr(0, 30);
         }
         var NotificationId = obj.getProp("NotificationId");
         var str = `
