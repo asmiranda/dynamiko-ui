fe9a2ccbf9c9379819d54a6c633164023902bbfc
Alex <niq77avie@yahoo.com>
2020-07-16 20:40:09 +0800

    adfdfs

--- js/mainJS.js
+++ js/mainJS.js
@@ -3,67 +3,7 @@ $(function () {
     mainSearchForm = '#searchTable';
     mainId = 'input.mainId';
     registeredModules = [];
-    QUICK_UPDATER_COUNTER=0;
-
-    Object.defineProperty(Object.prototype, "getPropDefault", {
-        value: function (prop, val) {
-            var retVal = val;
-            var key,self = this;
-            if (Object.keys(self).length > 0) {
-                for (key in self) {
-                    if (key.toLowerCase() == prop.toLowerCase()) {
-                        retVal = self[key];
-                        break;
-                    }
-                }
-            }
-            if (retVal==null || retVal=='null') {
-                retVal = val;
-            }
-            return retVal;
-        },
-        //this keeps jquery happy
-        enumerable: false
-    });
-    
-    Object.defineProperty(Object.prototype, "getProp", {
-        value: function (prop) {
-            var key,self = this;
-            for (key in self) {
-                if (key.toLowerCase() == prop.toLowerCase()) {
-                    return self[key];
-                }
-            }
-        },
-        //this keeps jquery happy
-        enumerable: false
-    });
-
-    Object.defineProperty(Object.prototype, "setProp", {
-        value: function (prop, val) {
-            var key,self = this;
-            var found = false;
-            if (Object.keys(self).length > 0) {
-                for (key in self) {
-                    if (key.toLowerCase() == prop.toLowerCase()) {
-                        //set existing property
-                        found = true;
-                        self[key] = val;
-                        break;
-                    }
-                }
-            }
-    
-            if (!found) {
-                //if the property was not found, create it
-                self[prop] = val;
-            }
-    
-            return val;
-        },
-        //this keeps jquery happy
-        enumerable: false
-    });
+    QUICK_UPDATER_COUNTER = 0;
 
     allTable = [];
     dynamikoFullScreen = new DynamikoFullScreen();
@@ -81,8 +21,8 @@ $(function () {
     formLinker = new FormLinker();
     userUI = new UserUI();
     myReportViewer = new MyReportViewer();
-    quickUpdater = new QuickUpdater();   
-    quickSearcher = new QuickSearcher();   
+    quickUpdater = new QuickUpdater();
+    quickSearcher = new QuickSearcher();
     dynaRegister = new DynaRegister();
     uploadDataFile = new UploadDataFile();
     // utils = new Utils();
--- js/mainObj.js
+++ js/mainObj.js
@@ -4,3 +4,66 @@ class AjaxRequestDTO {
         this.data = data;
     }
 }
+
+$(function () {
+    Object.defineProperty(Object.prototype, "getPropDefault", {
+        value: function (prop, val) {
+            var retVal = val;
+            var key, self = this;
+            if (Object.keys(self).length > 0) {
+                for (key in self) {
+                    if (key.toLowerCase() == prop.toLowerCase()) {
+                        retVal = self[key];
+                        break;
+                    }
+                }
+            }
+            if (retVal == null || retVal == 'null') {
+                retVal = val;
+            }
+            return retVal;
+        },
+        //this keeps jquery happy
+        enumerable: false
+    });
+
+    Object.defineProperty(Object.prototype, "getProp", {
+        value: function (prop) {
+            var key, self = this;
+            for (key in self) {
+                if (key.toLowerCase() == prop.toLowerCase()) {
+                    return self[key];
+                }
+            }
+        },
+        //this keeps jquery happy
+        enumerable: false
+    });
+
+    Object.defineProperty(Object.prototype, "setProp", {
+        value: function (prop, val) {
+            var key, self = this;
+            var found = false;
+            if (Object.keys(self).length > 0) {
+                for (key in self) {
+                    if (key.toLowerCase() == prop.toLowerCase()) {
+                        //set existing property
+                        found = true;
+                        self[key] = val;
+                        break;
+                    }
+                }
+            }
+
+            if (!found) {
+                //if the property was not found, create it
+                self[prop] = val;
+            }
+
+            return val;
+        },
+        //this keeps jquery happy
+        enumerable: false
+    });
+})
+
