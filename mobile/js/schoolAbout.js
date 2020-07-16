9ae417fccfa363328f254091dfb678d0b737b6ff
Alex <niq77avie@yahoo.com>
2020-07-16 15:41:12 +0800

    In Progress

--- mobile/js/schoolAbout.js
+++ mobile/js/schoolAbout.js
@@ -3,8 +3,13 @@ class SchoolAbout {
 
     }
 
-    init(token) {
-        alert(token);
+    init() {
+        window.ReactNativeWebView.postMessage("Loaded");
+        document.getElementById("callingMobile").innerHTML = "The javascript works?";
+        window.addEventListener("message", message => {
+            // alert(message.data);
+            document.getElementById("mobileResponse").innerHTML += `${message.data}`;
+        });
     }
 }
 
