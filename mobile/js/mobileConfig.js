7c63e7a884e67676961f0283a9446f0637b7628b
alex <niq77avie@yahoo.com>
2020-07-17 17:05:39 +0800

    In Progress

---
+++ mobile/js/mobileConfig.js
@@ -0,0 +1,22 @@
+var MAIN_URL = `http://10.0.2.2:8888`;
+var MAIN_SIGNAL_URL = `ws://10.0.2.2:888`;
+var MAIN_SIGNAL_HTTP_URL = `http://10.0.2.2:888`;
+
+// var MAIN_URL = `https://service.dynamikosoft.com:8443`;
+// var MAIN_SIGNAL_URL = `wss://service.dynamikosoft.com:4343`;
+// var MAIN_SIGNAL_HTTP_URL = `https://service.dynamikosoft.com:4343`;
+
+var TOKEN;
+var USERNAME;
+var PROFILENAME;
+var dynaRegister; 
+
+class Config {
+    getDateFormat() {
+        return 'yyyy-mm-dd';
+    }
+}
+
+$(function() {
+    config = new Config();
+})
--- mobile/js/schoolSchedules.js
+++ mobile/js/schoolSchedules.js
@@ -3,8 +3,51 @@ class SchoolSchedules {
 
     }
 
-    init(token) {
-        alert(token);
+    loadAnnouncements(data) {
+        let url = `${MAIN_URL}/api/generic/${sessionStorage.companyCode}/widget/StudentScheduleUI/getAnnouncements/${data}`;
+        let ajaxRequestDTO = new AjaxRequestDTO(url, "");
+        let successFunction = function (data) {
+            $("#announcementList").empty();
+            $(data).each(function (index, obj) {
+                let SchoolAnnouncementId = obj.getPropDefault("SchoolAnnouncementId", "--");
+                let announcement = obj.getPropDefault("announcement", "--");
+                let announcementDate = obj.getPropDefault("announcementDate", "--");
+                let announcementUrl = obj.getPropDefault("announcementUrl", "--");
+                let imageCss = "width: 444px; height: 350px;";
+                let boxCss = "width: 500px;";
+                let profileUrl = `${MAIN_URL}/api/generic/${sessionStorage.companyCode}/profilePic/SchoolAnnouncementUI/${SchoolAnnouncementId}`;
+                let str = `
+                    <div class="box box-widget" style="margin: 15px; ${boxCss}">
+                        <div class="box-body">
+                            <img class="img-responsive pad" src="${profileUrl}" alt="Photo" style="${imageCss}">
+
+                            <p style="padding: 20px;">${announcementDate} - ${announcement}</p>
+                        </div>
+                        <div class="box-footer box-comments">
+                            <a href="${announcementUrl}" target="_blank" class="btn btn-default btn-xs"><i class="fa fa-share"></i> Read More</a>
+                        </div>
+                    </div>
+                `;
+                $("#announcementList").append(str);
+            });
+        };
+        ajaxCaller.ajaxGet(ajaxRequestDTO, successFunction);
+    }
+
+    onReactMessage(data) {
+        document.getElementById("mobileResponse").innerHTML = data;
+        sessionStorage.companyCode = "TEST";
+        sessionStorage.token = data;
+        this.loadAnnouncements("");
+    }
+
+    init() {
+        let context = this;
+        window.ReactNativeWebView.postMessage("Loaded");
+
+        window.addEventListener("message", message => {
+            context.onReactMessage(message.data);
+        });
     }
 }
 
