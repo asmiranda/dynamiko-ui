6811af7ed70684e0e3e8499c6e942290c4a51c95
alex <niq77avie@yahoo.com>
2020-07-18 10:54:53 +0800

    In Progress

--- mobile/js/mobileConfig.js
+++ mobile/js/mobileConfig.js
@@ -1,6 +1,6 @@
 var MAIN_URL = `http://10.0.2.2:8888`;
-var MAIN_SIGNAL_URL = `ws://10.0.2.2:888`;
-var MAIN_SIGNAL_HTTP_URL = `http://10.0.2.2:888`;
+var MAIN_SIGNAL_URL = `ws://10.0.2.2:7788`;
+var MAIN_SIGNAL_HTTP_URL = `http://10.0.2.2:7788`;
 
 // var MAIN_URL = `https://service.dynamikosoft.com:8443`;
 // var MAIN_SIGNAL_URL = `wss://service.dynamikosoft.com:4343`;
