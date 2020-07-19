d2a0b711c5214f94acd8e7028fb39b29f4ece6fa
alex <niq77avie@yahoo.com>
2020-07-19 18:10:50 +0800

    In Progress

--- js/serverConfig.js
+++ js/serverConfig.js
@@ -1,12 +1,11 @@
-var server = "localhost";
-
+var server = "service.dynamikosoft.com";
 var MAIN_URL = `https://${server}:8443`;
 var MAIN_SIGNAL_URL = `wss://${server}:4343`;
 var MAIN_SIGNAL_HTTP_URL = `https://${server}:8443`;
 
-var MOBILE_MAIN_URL = `http://10.0.2.2:8888`;
-var MOBILE_MAIN_SIGNAL_URL = `ws://10.0.2.2:7788`;
-var MOBILE_MAIN_SIGNAL_HTTP_URL = `http://10.0.2.2:7788`;
+var MOBILE_MAIN_URL = `https://${server}:8443`;
+var MOBILE_MAIN_SIGNAL_URL = `wss://${server}:4343`;
+var MOBILE_MAIN_SIGNAL_HTTP_URL = `https://${server}:4343`;
 
 var dynaRegister;
 
