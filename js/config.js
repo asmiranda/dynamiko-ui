fcadc14d7a7b488df90a7ac4f56bcf2ae6d1a951
Alex Miranda <niq77avie@yahoo.com>
2020-08-03 20:41:33 +0800

    In Progress

--- js/config.js
+++ js/config.js
@@ -1,9 +1,9 @@
 var server = "localhost";
-var MAIN_URL = `http://${server}:5000`;
-var MAIN_SIGNAL_URL = `http://${server}:5000`;
+var MAIN_URL = `https://${server}:8778`;
+var MAIN_SIGNAL_URL = `https://${server}:8778`;
 
-var MOBILE_MAIN_URL = `http://10.0.2.2:5000`;
-var MOBILE_MAIN_SIGNAL_URL = `ws://10.0.2.2:5000`;
+var MOBILE_MAIN_URL = `https://10.0.2.2:8778`;
+var MOBILE_MAIN_SIGNAL_URL = `wss://10.0.2.2:8778`;
 
 var ANNOUNCEMENT_URL = "http://mobilewebui.dynamikosoft.com/"
 
