eb9a72aeccfd8142e12242d605ac892d87976d28
Alex <niq77avie@yahoo.com>
2020-08-06 21:09:35 +0800

    In Progress

--- js/config.js
+++ js/config.js
@@ -1,9 +1,9 @@
 var server = "localhost";
-var MAIN_URL = `https://${server}:8778`;
-var MAIN_SIGNAL_URL = `https://${server}:8778`;
+var MAIN_URL = `https://${server}:8888`;
+var MAIN_SIGNAL_URL = `https://${server}:8888`;
 
-var MOBILE_MAIN_URL = `https://10.0.2.2:8778`;
-var MOBILE_MAIN_SIGNAL_URL = `wss://10.0.2.2:8778`;
+var MOBILE_MAIN_URL = `https://10.0.2.2:8888`;
+var MOBILE_MAIN_SIGNAL_URL = `wss://10.0.2.2:8888`;
 
 var ANNOUNCEMENT_URL = "http://mobilewebui.dynamikosoft.com/"
 
