91fb064cb2f4d371e8a179244220ced0b9111156
Alex <niq77avie@yahoo.com>
2020-08-11 11:04:26 +0800

    In  Progress

--- js/configLocal.js
+++ js/configLocal.js
@@ -1,10 +1,6 @@
 var server = "service.dynamikosoft.com";
 var MAIN_URL = `https://${server}:8888`;
 var MAIN_SIGNAL_URL = `https://${server}:8888`;
-
-var MOBILE_MAIN_URL = `https://10.0.2.2:8888`;
-var MOBILE_MAIN_SIGNAL_URL = `wss://10.0.2.2:8888`;
-
 var ANNOUNCEMENT_URL = "http://mobilewebui.dynamikosoft.com/"
 
 var dynaRegister;
