de87648a21c1badea5297c9cf662ddd1549186dd
Alex <niq77avie@yahoo.com>
2020-08-16 11:10:43 +0800

    In Progress

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
-// var server = "service.dynamikosoft.com";
-var server = "localhost";
+var server = "dynamikosoft.com";
+// var server = "localhost";
 var MAIN_URL = `https://${server}:8888`;
 var MAIN_SIGNAL_URL = `https://${server}:8888`;
 var ANNOUNCEMENT_URL = "http://mobilewebui.dynamikosoft.com/"
