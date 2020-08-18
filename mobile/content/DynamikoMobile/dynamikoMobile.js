654fd5d681bdac6dba82c5354d8bdc73da5a79ce
Alex <niq77avie@yahoo.com>
2020-08-18 11:08:29 +0800

    DynamikoMobile

---
+++ mobile/content/DynamikoMobile/dynamikoMobile.js
@@ -0,0 +1,19 @@
+class DynamikoMobile {
+    loadExplore() {
+        var url = "https://dynamikosoft.com:7777/mobile/content/DynamikoMobile/exploreContent.html";
+        var ajaxRequestDTO = new AjaxRequestDTO(url, "");
+        var successCallback = function (data) {
+            $("#main").html(data);
+        };
+        ajaxCaller.ajaxGet(ajaxRequestDTO, successCallback);
+    }
+
+    loadNews() {
+        var url = "https://dynamikosoft.com:7777/mobile/content/DynamikoMobile/newsContent.html";
+        var ajaxRequestDTO = new AjaxRequestDTO(url, "");
+        var successCallback = function (data) {
+            $("#main").html(data);
+        };
+        ajaxCaller.ajaxGet(ajaxRequestDTO, successCallback);
+    }
+}
