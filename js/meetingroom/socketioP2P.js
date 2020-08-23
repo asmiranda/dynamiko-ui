30c7de4fe707f4fd1e240942bed84e51d942d5ad
Alex <niq77avie@yahoo.com>
2020-08-23 11:58:32 +0800

    In Progress

--- js/meetingroom/socketioP2P.js
+++ js/meetingroom/socketioP2P.js
@@ -1,11 +1,25 @@
 class SocketIOP2P {
     constructor() {
+        // this.peerConnectionConfig = {
+        //     'iceServers': [
+        //         { 'urls': 'stun:stun.services.mozilla.com' },
+        //         { 'urls': 'stun:stun.l.google.com:19302' },
+        //     ]
+        // };
         this.peerConnectionConfig = {
             'iceServers': [
-                { 'urls': 'stun:stun.services.mozilla.com' },
+                { 'url': 'stun:stun.services.mozilla.com' },
                 { 'urls': 'stun:stun.l.google.com:19302' },
+                // {
+                //     'url': 'turn:192.158.29.39:3478?transport=udp',
+                //     'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+                //     'username': '28224511:1379330808'
+                // },
+                {
+                    'url': 'turn:dynamikosoft.com:3478?transport=tcp'
+                }
             ]
-        };
+        }
     }
 
     clearConnections() {
