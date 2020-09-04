4f8e1a8a0333a89f45c9c7aecda96630198227c2
Alex <niq77avie@yahoo.com>
2020-09-04 18:01:20 +0800

    Init

--- js/config.js
+++ js/config.js
@@ -1,6 +1,9 @@
 // var server = "dynamikosoft.com";
 var server = window.location.hostname;
 var MAIN_URL = `https://${server}:8888`;
+if (server == "localhost" || server == "10.0.2.2") {
+    MAIN_URL = `http://${server}:8888`;
+}
 
 var dynaRegister;
 
