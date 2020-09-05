99717def10e79f93f097bc2d5523947afec22059
Alex <niq77avie@yahoo.com>
2020-09-05 13:39:00 +0800

    Init

--- js/docker-config.js
+++ js/docker-config.js
@@ -1,7 +1,7 @@
 // var server = "dynamikosoft.com";
 var server = window.location.hostname;
 var port = window.location.port;
-var servicePort = port + 1000;
+var servicePort = parseInt(port) + 1000;
 var MAIN_URL = `https://${server}:${servicePort}`;
 
 var dynaRegister;
