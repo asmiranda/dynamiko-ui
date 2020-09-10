baea69c442551ee2d439afba6ecc27b621335fb7
Alex <niq77avie@yahoo.com>
2020-09-10 21:45:22 +0800

    Fix Schedule

--- js/modules/submodules/SchoolScheduleUI.js
+++ js/modules/submodules/SchoolScheduleUI.js
@@ -14,21 +14,21 @@ class SchoolScheduleUI extends AbstractSubUI {
     }
 
     formatSearchList(index, obj, tabName) {
-        var accountName = obj.getProp("lastName") + ", " + obj.getProp("firstName");
-        var level = obj.getPropDefault("gradeLevel", "--");
-        var accountNumber = obj.getPropDefault("invoiceNumber", "--");
-        var email = obj.getPropDefault("email");
-        var AdmissionId = obj.getPropDefault("Id");
+        let facultyEmail = obj.getProp("facultyEmail");
+        let subjectCode = obj.getPropDefault("subjectCode", "--");
+        let startTime = obj.getPropDefault("startTime", "--");
+        let endTime = obj.getPropDefault("endTime");
+        let id = obj.getPropDefault("Id");
         var str = `
             <div style="display: flex; flex-wrap: wrap;">
                 <div style="flex: 100%;">
-                    <span><a href="#" class="${this.selectSearchRecord}" recordId="${AdmissionId}" module="${this.moduleName}" tabName="${tabName}">${accountName}</a></span>
-                    <span class="pull-right">Level: ${level}</span><br/>
+                    <span><a href="#" class="${this.selectSearchRecord}" recordId="${id}" module="${this.moduleName}" tabName="${tabName}">${facultyEmail}</a></span>
+                    <span class="pull-right"></span><br/>
                 </div>
                 <div style="flex: 100%;">
-                    <span><a href="#" class="${this.selectSearchRecord}" recordId="${AdmissionId}" module="${this.moduleName}" tabName="${tabName}">
-                        Invoice #: ${accountNumber}</a></span>
-                    <span class="pull-right" style="font-size: 14px;">${email}</span><br/>
+                    <span><a href="#" class="${this.selectSearchRecord}" recordId="${id}" module="${this.moduleName}" tabName="${tabName}">
+                        ${startTime} to ${endTime}</a></span>
+                    <span class="pull-right" style="font-size: 14px;">${subjectCode}</span><br/>
                 </div>
             </div>
             <hr style="margin-top: 5px; width: 98%">
